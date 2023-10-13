a38c7f5bc6f39dcf5e574eb8142b89c9e9484918
Oleksandr Kulkov <hutofsato@gmail.com>
2023-10-13 15:46:01 +0300

    Added layouts and changed some styles

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+    {
+        name: "Чайна Хатинка",
+        path: "/",
+    },
+    {
+        name: "Наші чаї",
+        path: "/teas/",
+    },
+    {
+        name: "Читальня",
+        path: "/library/",
+    },
+]
+
+export default navData;
